a492b7a10c517904a437f84a37b97435ff794266
merrylh <85388755+merrylh@users.noreply.github.com>
2021-06-16 15:35:28 +0530

    convert numbers into years and days
    Write a Java program to convert minutes into number of years and days.
    Test Data : Input the number of minutes: 3456789 
    In this program it converts the user input of minutes into years and days.

---
+++ Convertmin.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Convertmin {
+public static void main(String[] args) 
+{
+Scanner input = new Scanner(System.in);
+int a;
+System.out.println("Enter the input: ");
+a=input.nextInt();
+int year,days;
+int cn=60*24*365;
+year=a/cn;
+System.out.print(year+" years ");
+a=a-(year*cn);
+int cm=60*24;
+days=a/cm;
+System.out.print("and "+days+" days.");
+}
+}
